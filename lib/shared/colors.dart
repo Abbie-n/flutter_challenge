f523ae3175364240eb671d64249825e8318e343e
AbbieN <52113711+Abbie-n@users.noreply.github.com>
2022-02-14 04:15:55 +0100

    Created some shared classes. eg colors, theme

---
+++ lib/shared/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color.fromRGBO(10, 10, 15, 1);
+  static const minTextColor = Color.fromRGBO(112, 112, 131, 1);
+  static const neutrals = Color.fromRGBO(252, 252, 253, 1);
+  static const white = Color.fromRGBO(255, 255, 255, 1);
+}
