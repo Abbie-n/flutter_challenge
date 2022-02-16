346f78fcdad31e4888770dc3618f02845e64810c
AbbieN <52113711+Abbie-n@users.noreply.github.com>
2022-02-16 22:15:47 +0100

    Network call setup

---
+++ lib/core/utils/constants.dart
@@ -0,0 +1,4 @@
+const String baseUrl = 'https://api.pexels.com/v1/curated?page=0&per_page=';
+const String limit = '10';
+const String apiKey =
+    '563492ad6f91700001000001618430f2bc1040abb7708fe17836fdd5';
